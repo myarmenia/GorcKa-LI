94472c396c0ef32a354fb1d863ba209fb20c4dee
ArmineKhachatryan1982 <armine.khachatryan1982@gmail.com>
2025-02-14 10:27:35 +0400

    add location translation seeder 14.02.25

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -26,6 +26,7 @@ class DatabaseSeeder extends Seeder
             CreateAdminUserSeeder::class,
             LangSeeder::class,
             LocationSeeder::class,
+            LocationTranslationSeeder::class
 
         ]);
     }
