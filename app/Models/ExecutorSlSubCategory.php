22161f209267ee53dc8f5b366e89adaa04aca275
NarineHay <naromisho87@gmail.com>
2025-06-16 13:36:01 +0400

    changes specialists

--- app/Models/ExecutorSlSubCategory.php
+++ app/Models/ExecutorSlSubCategory.php
@@ -2,9 +2,42 @@
 
 namespace App\Models;
 
+use App\Traits\FilterTrait;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
 
 class ExecutorSlSubCategory extends Model
 {
-    //
+    use FilterTrait;
+    protected $guarded = [];
+
+    protected $defaultFields = ['sub_category_id'];
+
+    protected $relationFilter = [
+        'user' => ['location_id', 'name', 'status']
+    ];
+
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function activeUser()
+    {
+        return $this->belongsTo(User::class, 'user_id')->where('status', 1);
+    }
+
+    public function sub_category(): BelongsTo
+    {
+        return $this->belongsTo(SubCategory::class, 'sub_category_id');
+    }
+
+
+
+    public function filables(): MorphMany
+    {
+        return $this->morphMany(Filable::class, 'filable');
+    }
 }
