7f7c4193516d2247cb2a95a6b1e77d4c16cd79ea
ArmineKhachatryan1982 <armine.khachatryan1982@gmail.com>
2025-07-28 15:16:26 +0400

    add webp extension to single job page

---
+++ resources/js/Pages/User/Home/TopSpecialist.vue
@@ -0,0 +1,31 @@
+
+<script setup>
+
+</script>
+<template>
+    <section>
+       <!-- <div class="mt-5">
+                                <div class="grid items-center grid-cols-12">
+                                    <div class="col-span-12 lg:col-span-8">
+                                        <h6 class="mb-0 text-gray-900 fs-16 dark:text-gray-50"> Showing 1 – 8 of 11 results </h6>
+                                    </div>
+                                    <div class="col-span-12 lg:col-span-4">
+                                        <div class="grid grid-cols-12 gap-y-3 lg:gap-3">
+                                            <div class="col-span-12 lg:col-span-6">
+                                                <div class="mt-2 selection-widget mt-lg-0">
+                                                    <div class="choices" data-type="select-one" tabindex="0" role="combobox" aria-autocomplete="list" aria-haspopup="true" aria-expanded="false"><div class="choices__inner"><select class="form-select choices__input" data-trigger="" name="choices-single-filter-orderby" id="choices-single-filter-orderby" aria-label="Default select example" hidden="" tabindex="-1" data-choice="active"><option value="df" data-custom-properties="[object Object]">Default</option></select><div class="choices__list choices__list--single"><div class="choices__item choices__item--selectable" data-item="" data-id="1" data-value="df" data-custom-properties="[object Object]" aria-selected="true">Default</div></div></div><div class="choices__list choices__list--dropdown" aria-expanded="false"><input type="search" name="search_terms" class="choices__input choices__input--cloned" autocomplete="off" autocapitalize="off" spellcheck="false" role="textbox" aria-autocomplete="list" aria-label="null" placeholder=""><div class="choices__list" role="listbox"><div id="choices--choices-single-filter-orderby-item-choice-1" class="choices__item choices__item--choice is-selected choices__item--selectable is-highlighted" role="option" data-choice="" data-id="1" data-value="df" data-select-text="Press to select" data-choice-selectable="" aria-selected="true">Default</div><div id="choices--choices-single-filter-orderby-item-choice-2" class="choices__item choices__item--choice choices__item--selectable" role="option" data-choice="" data-id="2" data-value="ne" data-select-text="Press to select" data-choice-selectable="">Newest</div><div id="choices--choices-single-filter-orderby-item-choice-3" class="choices__item choices__item--choice choices__item--selectable" role="option" data-choice="" data-id="3" data-value="od" data-select-text="Press to select" data-choice-selectable="">Oldest</div><div id="choices--choices-single-filter-orderby-item-choice-4" class="choices__item choices__item--choice choices__item--selectable" role="option" data-choice="" data-id="4" data-value="rd" data-select-text="Press to select" data-choice-selectable="">Random</div></div></div></div>
+                                                </div>
+                                            </div>
+                                            <div class="col-span-12 lg:col-span-6">
+                                                <div class="mt-2 selection-widget mt-lg-0">
+                                                    <div class="choices" data-type="select-one" tabindex="0" role="combobox" aria-autocomplete="list" aria-haspopup="true" aria-expanded="false"><div class="choices__inner"><select class="form-select choices__input" data-trigger="" name="choices-candidate-page" id="choices-candidate-page" aria-label="Default select example" hidden="" tabindex="-1" data-choice="active"><option value="df" data-custom-properties="[object Object]">All</option></select><div class="choices__list choices__list--single"><div class="choices__item choices__item--selectable" data-item="" data-id="1" data-value="df" data-custom-properties="[object Object]" aria-selected="true">All</div></div></div><div class="choices__list choices__list--dropdown" aria-expanded="false"><input type="search" name="search_terms" class="choices__input choices__input--cloned" autocomplete="off" autocapitalize="off" spellcheck="false" role="textbox" aria-autocomplete="list" aria-label="null" placeholder=""><div class="choices__list" role="listbox"><div id="choices--choices-candidate-page-item-choice-1" class="choices__item choices__item--choice choices__item--selectable is-highlighted" role="option" data-choice="" data-id="1" data-value="ne" data-select-text="Press to select" data-choice-selectable="" aria-selected="true">8 per Page</div><div id="choices--choices-candidate-page-item-choice-2" class="choices__item choices__item--choice choices__item--selectable" role="option" data-choice="" data-id="2" data-value="ne" data-select-text="Press to select" data-choice-selectable="">12 per Page</div><div id="choices--choices-candidate-page-item-choice-3" class="choices__item choices__item--choice is-selected choices__item--selectable" role="option" data-choice="" data-id="3" data-value="df" data-select-text="Press to select" data-choice-selectable="">All</div></div></div></div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div> -->
+
+    </section>
+
+</template>
